38110314f72b048a0f31d7f97a3fc5dd8fc97b7d
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-12 23:51:45 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -91,7 +91,7 @@ const Index = () => {
   }, [nextSlide, isMenuOpen]);
 
   return (
-    <div className="relative min-h-screen min-h-[400dvh] w-full overflow-hidden">
+    <div className="relative min-h-[100dvh] w-full overflow-hidden">
       {/* Background Images with Transition */}
       {slides.map((slide, index) => (
         <div
